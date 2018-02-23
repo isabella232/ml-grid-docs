645ef3b40e375381f42a3aad9c475433dd938503
Olaf Veerman <olaf.veerman@gmail.com>
2018-02-23 14:34:15 -0500

    Minor fixes

--- _config.js
+++ _config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  baseurl: 'http://devseed.com/hv-grid-docs',
+  baseurl: 'http://devseed.com/ml-grid-docs',
 
   title: 'Mapping the electric grid',
-  description: 'Using machine learning to augment human tracing of high-voltage infrastructure',
+  description: 'Using ML to augment human tracing of HV infrastructure',
   sectionsMeta: {
-    's0-summary': 'Summary'
+    's0-summary': 'Summary',
     's1-introduction': 'Introduction',
-    's2-methodology': 'Methodology'
-    's3-results': 'Results'
+    's2-methodology': 'Methodology',
+    's3-results': 'Results',
     's4-discussion': 'Discussion'
   },
   attribution: {
     name: 'World Bank Group',
-    //role: 'Developed for World Bank',
-    logo: 'wbg-logo-hor-col-grad.png',
-    //url: 'https://github.com/developmentseed/ui-seed/'
+    role: 'Developed for World Bank',
+    logo: 'credits/wbg-logo-hor-col-grad.png',
+    url: 'https://github.com/developmentseed/ui-seed/'
   }
 };
