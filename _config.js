9556cf16087f445272c9d334338ddbad4bd24b82
Mark Wronkiewicz <wronk@developmentseed.org>
2018-02-23 18:14:39 +0100

    Initial commit

---
+++ _config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  baseurl: 'http://devseed.com/hv-grid-docs',
+
+  title: 'Mapping the electric grid',
+  description: 'Using machine learning to augment human tracing of high-voltage infrastructure',
+  sectionsMeta: {
+    's0-summary': 'Summary'
+    's1-introduction': 'Introduction',
+    's2-methodology': 'Methodology'
+    's3-results': 'Results'
+    's4-discussion': 'Discussion'
+  },
+  attribution: {
+    name: 'World Bank Group',
+    //role: 'Developed for World Bank',
+    logo: 'wbg-logo-hor-col-grad.png',
+    //url: 'https://github.com/developmentseed/ui-seed/'
+  }
+};
---
+++ app/posts/s0-summary/0-0-summary/0-0-0-index.md
@@ -0,0 +1,8 @@
+---
+title: Executive Summary
+date: 2018-02-16
+layout: post.html
+permalink: /
+---
+
+# Executive Summary
